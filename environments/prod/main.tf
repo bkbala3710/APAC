7aa62821d9c35e6808e376214009409a9e769b00
Balakumaran T <bkbala3710@gmail.com>
2025-11-20 20:41:51 +0530

    Simplify VPC module configuration
    Removed subnet_cidr and subnet_az from VPC module configuration.

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -2,8 +2,7 @@
 module "vpc" {
   source        = "../../modules/vpc"
   vpc_cidr      = "10.0.0.0/16"
-  subnet_cidr   = "10.0.1.0/24"
-  subnet_az     = "${var.aws_region}a"          # ap-southeast-1a
   env           = "dev"
 }
+
 
