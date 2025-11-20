0da7ae99565eb8aced9a8203b65132ee92e39dcf
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:10:57 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,8 +1,9 @@
 module "vpc" {
-  source = "../.."  # update path based on your folder
+  source = "../../modules/vpc"  # update path based on your folder
 
   vpc_name     = "PROD-APAC-VPC"
   vpc_cidr     = "10.0.0.0/16"
   subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   azs          = ["ap-south-1a", "ap-south-1b"]
 }
+
