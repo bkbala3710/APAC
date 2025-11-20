5a2dac69b6af6b591b6f0fb5def964652be279ec
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:07:14 +0530

    Replace VPC module with direct resource definitions

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,12 +1,22 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-module "vpc" {
-  source        = "../../modules/vpc"
-  vpc_cidr      = "10.0.0.0/16"
-  env           = "PROD-APAC"
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidrs)
 
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = var.map_public_ip
 
-
-
-
+  tags = {
+    Name = "${var.vpc_name}-subnet-${count.index + 1}"
+  }
+}
