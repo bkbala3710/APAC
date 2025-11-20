71d3a4f57aec4c66bba380639f18a7f9459458e1
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:09:02 +0530

    Refactor VPC and subnet resources into a module
    Replaced individual VPC and subnet resources with a VPC module for better structure.

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,22 +1,8 @@
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+module "vpc" {
+  source = "../.."  # update path based on your folder
 
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
-resource "aws_subnet" "subnets" {
-  count = length(var.subnet_cidrs)
-
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidrs[count.index]
-  availability_zone       = var.azs[count.index]
-  map_public_ip_on_launch = var.map_public_ip
-
-  tags = {
-    Name = "${var.vpc_name}-subnet-${count.index + 1}"
-  }
+  vpc_name     = "PROD-APAC-VPC"
+  vpc_cidr     = "10.0.0.0/16"
+  subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs          = ["ap-south-1a", "ap-south-1b"]
 }
