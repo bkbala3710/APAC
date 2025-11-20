0e353f24171e3b88f305771d9b21e555412343be
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:05:42 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,18 +1,22 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.env}-VPC"
+    Name = var.vpc_name
   }
 }
 
-resource "aws_subnet" "public" {
-  for_each = toset(var.public_subnets)
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = each.value
-  availability_zone = element(var.availability_zones, index(var.public_subnets, each.value))
-  map_public_ip_on_launch = true
-  tags = { Name = "public-${each.value}" }
-}
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidrs)
 
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = var.map_public_ip
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-${count.index + 1}"
+  }
+}
