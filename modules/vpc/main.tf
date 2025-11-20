9c0caa2468f349b1b8807aeec88610f491734de2
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:36:44 +0530

    Refactor VPC and subnet resources in main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,15 +8,28 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "subnets" {
+resource "aws_subnet" "subnets-1" {
   count = length(var.subnet_cidrs)
 
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidrs[count.index]
-  availability_zone       = var.azs[count.index]
-  map_public_ip_on_launch = var.map_public_ip
+  cidr_block              = var.subnet_cidrs-1[count.index]
+  availability_zone       = var.azs-1[count.index]
+  map_public_ip_on_launch = var.map_public_ip-1
 
   tags = {
-    Name = "${var.vpc_name}-subnet-${count.index + 1}"
+    Name = "${var.vpc_name}-Public-subnet-${count.index + 1}"
+  }
+
+resource "aws_subnet" "subnets-2" {
+  count = length(var.subnet_cidrs)
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidrs-2[count.index]
+  availability_zone       = var.azs-2[count.index]
+  map_public_ip_on_launch = var.map_public_ip-2
+
+  tags = {
+    Name = "${var.vpc_name}-Private-subnet-${count.index + 1}"
   }
 }
+
