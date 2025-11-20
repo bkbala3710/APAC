a5a6d1b250c675d523065388805637ae1019e342
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:33:14 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -3,8 +3,11 @@ module "vpc" {
 
   vpc_name     = "PROD-APAC-VPC"
   vpc_cidr     = "10.0.0.0/16"
-  subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-  azs          = ["ap-southeast-1a", "ap-southeast-1b"]
+  subnet_cidrs-1 = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs-1         = ["ap-southeast-1a", "ap-southeast-1b"]
+  subnet_cidrs-2 = ["10.0.11.0/24", "10.0.12.0/24"]
+  azs-2         = ["ap-southeast-1a", "ap-southeast-1b"]
 }
+
 
 
