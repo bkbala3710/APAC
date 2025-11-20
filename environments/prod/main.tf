f56a8519e3cf8540d56bd518bcc0c98f2baae9e1
Balakumaran T <bkbala3710@gmail.com>
2025-11-20 20:33:51 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -7,11 +7,3 @@ module "vpc" {
   env           = "dev"
 }
 
-module "ec2" {
-  source            = "../../modules/ec2"
-  ami_id            = "ami-0afc7fe9be84307e4"
-  instance_type     = "t2.micro"
-  subnet_id         = module.vpc.subnet_id
-  ec2_count         = 2
-  env               = "dev"
-}
