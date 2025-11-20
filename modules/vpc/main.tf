4de53f827171f997fdfcd46a7e21e3c123c033f1
Balakumaran T <bkbala3710@gmail.com>
2025-11-20 23:49:07 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,9 +9,10 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "public" {
   for_each = toset(var.public_subnets)
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = aws_vpc.myvpc.id
   cidr_block        = each.value
   availability_zone = element(var.availability_zones, index(var.public_subnets, each.value))
   map_public_ip_on_launch = true
   tags = { Name = "public-${each.value}" }
 }
+
