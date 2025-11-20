307729e83e27677352a8a7e6f6bed5a5f25f19eb
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:05:51 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,3 +1,27 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
+variable "vpc_name" {
+  type        = string
+  default     = "my-vpc"
+  description = "Name of the VPC"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+}
+
+variable "subnet_cidrs" {
+  type        = list(string)
+  description = "List of subnet CIDRs"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "List of availability zones for the subnets"
+}
+
+variable "map_public_ip" {
+  type        = bool
+  default     = true
+  description = "Assign public IP on launch?"
 }
