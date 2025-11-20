96164eccc4148379b7d9f98b32f4d3559bc34e6d
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 00:38:39 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -3,5 +3,9 @@ output "vpc_id" {
 }
 
 output "subnet_ids" {
-  value = aws_subnet.subnets[*].id
+  value = aws_subnet.subnets-1[*].id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.subnets-2[*].id
 }
