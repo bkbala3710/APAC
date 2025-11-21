eca98b6fd147218fb2a88fda39315d3ebbf07c89
Balakumaran T <bkbala3710@gmail.com>
2025-11-21 10:03:27 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -13,30 +13,25 @@ resource "aws_instance" "bastion_host_1" {
 
 resource "aws_security_group" "prod_bastion_host_sg_1" {
   name        = var.name_sg_1
-  description = "Security group for web server"
+  description = "Security group for bastion host"
   vpc_id      = var.vpc_id
 
   ingress {
-    content {
-      description      = "Allow SSH"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    for_each = var.egress_rules
-    content {
-      description      = "Allow SSH"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+    description      = "Allow all outbound"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
